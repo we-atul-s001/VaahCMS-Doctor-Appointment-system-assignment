b8b86a905c61caeb35a2e80a0f3a5cf182c14253
Atul Pratap Singh <atul-s001@webreinvent.com>
2024-09-13 18:38:12 +0530

    Added: Patient Curd Setup

--- vaahcms/VaahCms/Modules/Appointment/Vue/routes/routes.js
+++ vaahcms/VaahCms/Modules/Appointment/Vue/routes/routes.js
@@ -2,8 +2,10 @@ let routes= [];
 
 import dashboard from "./vue-routes-dashboard";
 import doctor from "./vue-routes-doctors";
+import patient from "./vue-routes-patients";
 
 routes = routes.concat(dashboard);
 routes = routes.concat(doctor);
+routes = routes.concat(patient);
 
 export default routes;
