894eb38c1462f7f35aafc1d7b58b9cf47966de00
Atul Pratap Singh <atul-s001@webreinvent.com>
2024-09-26 11:00:18 +0530

    Added: Status column in the migration

---
+++ vaahcms/VaahCms/Modules/Appointment/Database/Migrations/2024_09_26_105419_add_column_status_in_appointment.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnStatusInAppointment extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::table('vh_appointments', function (Blueprint $table) {
+
+            //----/common fields
+            $table->boolean('status')->nullable()->index();
+
+        });
+    }
+
+    /**
+    * Reverse the migrations.
+    *
+    * @return void
+    */
+    public function down()
+    {
+        Schema::dropIfExists('add_column_status_in_appointment');
+    }
+}
