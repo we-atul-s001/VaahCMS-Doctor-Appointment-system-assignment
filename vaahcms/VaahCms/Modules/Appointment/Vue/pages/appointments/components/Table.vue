d30dca0a9505897e64dc6ae5d6a9cf1a31b7e274
Atul Pratap Singh <atul-s001@webreinvent.com>
2024-09-27 14:02:47 +0530

    Updated: Update button hide of appointment on doctor

--- vaahcms/VaahCms/Modules/Appointment/Vue/pages/appointments/components/Table.vue
+++ vaahcms/VaahCms/Modules/Appointment/Vue/pages/appointments/components/Table.vue
@@ -127,7 +127,7 @@ function formatTimeWithAmPm(time) {
 
                         <Button class="p-button-tiny p-button-text"
                                 data-testid="appoinments-table-to-edit"
-                                v-if="prop.data.status !== 0"
+                                v-if="prop.data.status !== 0 && store.hasPermission(store.assets.permission, 'appointment-has-access-of-patient')"
                                 v-tooltip.top="'Update'"
                                 @click="store.toEdit(prop.data)"
                                 icon="pi pi-pencil" />
