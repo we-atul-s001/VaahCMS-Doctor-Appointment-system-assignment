5c1c020d7838e4a3fdefb2227bce7b0668771a43
Atul Pratap Singh <atul-s001@webreinvent.com>
2024-10-04 18:18:55 +0530

    Fixed: Time format in the update form of doctor and appointment

--- vaahcms/VaahCms/Modules/Appointment/Vue/pages/doctors/Form.vue
+++ vaahcms/VaahCms/Modules/Appointment/Vue/pages/doctors/Form.vue
@@ -1,10 +1,8 @@
 <script setup>
-import {computed, onMounted, ref, watch} from "vue";
-import { useDoctorStore } from '../../stores/store-doctors'
-
-import VhField from './../../vaahvue/vue-three/primeflex/VhField.vue'
-import {useRoute} from 'vue-router';
-
+import { computed, onMounted, ref } from "vue";
+import { useDoctorStore } from '../../stores/store-doctors';
+import VhField from './../../vaahvue/vue-three/primeflex/VhField.vue';
+import { useRoute } from 'vue-router';
 
 const store = useDoctorStore();
 const route = useRoute();
@@ -13,9 +11,7 @@ onMounted(async () => {
     /**
      * Fetch the record from the database
      */
-    if((!store.item || Object.keys(store.item).length < 1)
-            && route.params && route.params.id)
-    {
+    if ((!store.item || Object.keys(store.item).length < 1) && route.params && route.params.id) {
         await store.getItem(route.params.id);
     }
 
@@ -28,18 +24,41 @@ const toggleFormMenu = (event) => {
     form_menu.value.toggle(event);
 };
 //--------/form_menu
-const isValidTime = (date) => date instanceof Date && !isNaN(date.getTime());
+
+
+const convertTo12HourFormat = (time) => {
+    if (!time) return '';
+
+    const [hours, minutes] = time.split(':').map(Number);
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    const formattedHours = hours % 12 || 12;
+    return `${formattedHours}:${minutes < 10 ? '0' + minutes : minutes} ${ampm}`;
+};
+
+const formattedShiftStartTime = computed({
+    get() {
+        return convertTo12HourFormat(store.item?.shift_start_time);
+    },
+    set(value) {
+        store.item.shift_start_time = value;
+    }
+});
+
+const formattedShiftEndTime = computed({
+    get() {
+        return convertTo12HourFormat(store.item?.shift_end_time);
+    },
+    set(value) {
+        store.item.shift_end_time = value;
+    }
+});
 
 </script>
+
 <template>
-
-    <div class="col-6" >
-
+    <div class="col-6">
         <Panel class="is-small">
-
             <template class="p-1" #header>
-
-
                 <div v-if="store.assets.permission.includes('appointment-has-access-of-doctor')" class="flex flex-row">
                     <div class="p-panel-title">
                         <span v-if="store.item && store.item.id">
@@ -49,17 +68,11 @@ const isValidTime = (date) => date instanceof Date && !isNaN(date.getTime());
                             Create
                         </span>
                     </div>
-
                 </div>
-
-
             </template>
 
             <template #icons>
-
-
                 <div class="p-inputgroup">
-
                     <Button class="p-button-sm"
                             v-tooltip.left="'View'"
                             v-if="store.item && store.item.id"
@@ -81,7 +94,6 @@ const isValidTime = (date) => date instanceof Date && !isNaN(date.getTime());
                             data-testid="doctors-create-and-new"
                             icon="pi pi-save"/>
 
-
                     <!--form_menu-->
                     <Button
                         type="button"
@@ -96,33 +108,24 @@ const isValidTime = (date) => date instanceof Date && !isNaN(date.getTime());
                           :popup="true" />
                     <!--/form_menu-->
 
-
                     <Button class="p-button-primary p-button-sm"
                             icon="pi pi-times"
                             data-testid="doctors-to-list"
                             @click="store.toList()">
                     </Button>
                 </div>
-
-
-
             </template>
 
-
             <div v-if="store.item" class="mt-2">
-
                 <Message severity="error"
                          class="p-container-message mb-3"
                          :closable="false"
                          icon="pi pi-trash"
                          v-if="store.item.deleted_at">
-
                     <div class="flex align-items-center justify-content-between">
-
                         <div class="">
                             Deleted {{store.item.deleted_at}}
                         </div>
-
                         <div class="ml-3">
                             <Button label="Restore"
                                     class="p-button-sm"
@@ -130,11 +133,8 @@ const isValidTime = (date) => date instanceof Date && !isNaN(date.getTime());
                                     @click="store.itemAction('restore')">
                             </Button>
                         </div>
-
                     </div>
-
                 </Message>
-
 
                 <VhField label="Name">
                     <div class="p-inputgroup">
@@ -148,16 +148,6 @@ const isValidTime = (date) => date instanceof Date && !isNaN(date.getTime());
                     </div>
                 </VhField>
 
-<!--                <VhField label="Slug">-->
-<!--                    <div class="p-inputgroup">-->
-<!--                        <InputText class="w-full"-->
-<!--                                   placeholder="Enter the slug"-->
-<!--                                   name="doctors-slug"-->
-<!--                                   data-testid="doctors-slug"-->
-<!--                                   v-model="store.item.slug" required/>-->
-<!--                        <div class="required-field hidden"></div>-->
-<!--                    </div>-->
-<!--                </VhField>-->
                 <VhField label="Email">
                     <div class="p-inputgroup">
                         <InputText class="w-full"
@@ -168,17 +158,18 @@ const isValidTime = (date) => date instanceof Date && !isNaN(date.getTime());
                         <div class="required-field hidden"></div>
                     </div>
                 </VhField>
+
                 <VhField label="Phone">
-                <div class="p-inputgroup">
-                    <InputNumber class="w-full"
-                                 placeholder="Enter the Phone"
-                                 name="doctors-phone"
-                                 :useGrouping="false"
-                                 data-testid="doctors-phone"
-                                 v-model="store.item.phone" required/>
-                    <div class="required-field hidden"></div>
-                </div>
-            </VhField>
+                    <div class="p-inputgroup">
+                        <InputNumber class="w-full"
+                                     placeholder="Enter the Phone"
+                                     name="doctors-phone"
+                                     :useGrouping="false"
+                                     data-testid="doctors-phone"
+                                     v-model="store.item.phone" required/>
+                        <div class="required-field hidden"></div>
+                    </div>
+                </VhField>
 
                 <VhField label="Specialization">
                     <div class="p-inputgroup">
@@ -190,28 +181,26 @@ const isValidTime = (date) => date instanceof Date && !isNaN(date.getTime());
                         <div class="required-field hidden"></div>
                     </div>
                 </VhField>
+
                 <VhField label="Working Hour">
                     <div class="p-inputgroup">
                         <Calendar
-                            v-model="store.item.shift_start_time"
+                            v-model="formattedShiftStartTime"
                             showTime
                             hourFormat="12"
-                            store.item.shift_start_time
-                           time-only
+                            time-only
                             placeholder="Shift Start Time"
                         />
                         <Calendar
-                            v-model="store.item.shift_end_time"
+                            v-model="formattedShiftEndTime"
                             showTime
                             hourFormat="12"
-                            store.item.shift_end_time
                             time-only
                             placeholder="Shift End Time"
                         />
                         <div class="required-field hidden"></div>
                     </div>
                 </VhField>
-
 
                 <VhField label="Is Active">
                     <InputSwitch v-bind:false-value="0"
@@ -221,10 +210,7 @@ const isValidTime = (date) => date instanceof Date && !isNaN(date.getTime());
                                  data-testid="doctors-active"
                                  v-model="store.item.is_active"/>
                 </VhField>
-
             </div>
         </Panel>
-
     </div>
-
 </template>
