46a7e4a175ae72a1633b3ab9133ec1f955cfb7f8
Atul Pratap Singh <atul-s001@webreinvent.com>
2024-10-08 16:16:20 +0530

    Added: Price Column added in doctor

---
+++ vaahcms/VaahCms/Modules/Appointment/Database/Migrations/2024_10_08_160519_add_column_price_in_vh_doctors.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnPriceInVhDoctors extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::table('vh_doctors', function (Blueprint $table) {
+            $table->integer('price')->nullable()->index();
+            //----/common fields
+
+        });
+    }
+
+    /**
+    * Reverse the migrations.
+    *
+    * @return void
+    */
+    public function down()
+    {
+        Schema::dropIfExists('price');
+    }
+}
