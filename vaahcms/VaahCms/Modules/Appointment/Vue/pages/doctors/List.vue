7ba120e18272650b7e5771a1c2fc3254954fbdba
Atul Pratap Singh <atul-s001@webreinvent.com>
2024-10-14 18:18:42 +0530

    Added: Dialog box appear when file will uploading

--- vaahcms/VaahCms/Modules/Appointment/Vue/pages/doctors/List.vue
+++ vaahcms/VaahCms/Modules/Appointment/Vue/pages/doctors/List.vue
@@ -10,12 +10,12 @@ import Table from "./components/Table.vue";
 import Filters from './components/Filters.vue';
 import DoctorFilter from './components/DoctorFilter.vue';
 
+import { useConfirm } from "primevue/useconfirm";
+const confirm = useConfirm();
+
 const store = useDoctorStore();
 const root = useRootStore();
 const route = useRoute();
-
-import { useConfirm } from "primevue/useconfirm";
-const confirm = useConfirm();
 
 onMounted(async () => {
     document.title = 'Doctors - Appointment';
@@ -36,7 +36,6 @@ const toggleCreateMenu = (event) => {
 };
 //--------/form_menu
 
-
 const dynamicClass = computed(() => {
     if (store.quick_filters_doctors) {
         return 'col-9';
@@ -45,13 +44,11 @@ const dynamicClass = computed(() => {
     }
 });
 
-
 watch(() => store.show_filters, (newValue) => {
     if (newValue) {
         store.quick_filters_doctors = false;
     }
 });
-
 
 watch(() => store.quick_filters_doctors, (newValue) => {
     if (newValue) {
@@ -59,11 +56,13 @@ watch(() => store.quick_filters_doctors, (newValue) => {
     }
 });
 
-const fileInput = ref(null);
+const isDialogVisible = ref(false);
 
 const openFileDialog = () => {
-    fileInput.value.click();
+    isDialogVisible.value = true;
 };
+
+const fileInput = ref(null);
 
 const handleFileUpload = (event) => {
     const file = event.target.files[0];
@@ -74,6 +73,7 @@ const handleFileUpload = (event) => {
             const json_data = csvToJson(contents);
             console.log('Parsed JSON data:', json_data);
             importDoctors(json_data);
+            isDialogVisible.value = false;
         };
         reader.readAsText(file);
     }
@@ -97,91 +97,88 @@ const csvToJson = (csv) => {
 
 const exportDoctors = () => {
     store.exportDoctors();
-}
+};
 
 const importDoctors = (json_data) => {
     store.importDoctors(json_data);
-}
+};
 </script>
-<template>
 
+<template>
     <div class="grid" v-if="store.assets">
         <div :class="dynamicClass">
             <Panel class="is-small">
 
                 <template class="p-1" #header>
-
                     <div class="flex flex-row">
-                        <div >
+                        <div>
                             <b class="mr-1">Doctors</b>
-                            <Badge v-if="store.list && store.list.total > 0"
-                                   :value="store.list.total">
-                            </Badge>
+                            <Badge v-if="store.list && store.list.total > 0" :value="store.list.total" />
                         </div>
-
                     </div>
-
                 </template>
 
                 <template #icons>
-
                     <div class="p-inputgroup">
+                        <Button v-if="store.assets.permission.includes('appointment-has-access-of-patient')" data-testid="doctors-list-create"
+                                class="p-button-sm"
+                                @click="store.toForm()">
+                            <i class="pi pi-plus mr-1"></i>
+                            Create
+                        </Button>
 
-                    <Button v-if="store.assets.permission.includes('appointment-has-access-of-patient')" data-testid="doctors-list-create"
-                            class="p-button-sm"
-                            @click="store.toForm()">
-                        <i class="pi pi-plus mr-1"></i>
-                        Create
-                    </Button>
+                        <!-- Upload Doctors CSV Button -->
+                        <Button @click="openFileDialog">Upload Doctors CSV</Button>
 
-                        <div class="card">
-                            <Button @click="openFileDialog">Upload Doctors CSV</Button>
-                            <input type="file" ref="fileInput" @change="handleFileUpload" accept=".csv" style="display: none;" />
-                            <Button label="Export Doctors"
-                                    @click="exportDoctors"
-                                    style="margin-left: 5px;"
-                            />
-                        </div>
-                    <Button data-testid="doctors-list-reload"
-                            class="p-button-sm"
-                            @click="store.getList()">
-                        <i class="pi pi-refresh mr-1"></i>
-                    </Button>
+                        <!-- Export Doctors Button -->
+                        <Button label="Export Doctors"
+                                @click="exportDoctors"
+                                style="margin-left: 5px;"
+                        />
 
-                    <!--form_menu-->
+                        <!-- Reload Button -->
+                        <Button data-testid="doctors-list-reload"
+                                class="p-button-sm"
+                                @click="store.getList()">
+                            <i class="pi pi-refresh mr-1"></i>
+                        </Button>
 
-                    <Button v-if="root.assets && root.assets.module
-                                                && root.assets.module.is_dev"
-                        type="button"
-                        @click="toggleCreateMenu"
-                        class="p-button-sm"
-                        data-testid="doctors-create-menu"
-                        icon="pi pi-angle-down"
-                        aria-haspopup="true"/>
+                        <!-- form_menu -->
+                        <Button v-if="root.assets && root.assets.module && root.assets.module.is_dev"
+                                type="button"
+                                @click="toggleCreateMenu"
+                                class="p-button-sm"
+                                data-testid="doctors-create-menu"
+                                icon="pi pi-angle-down"
+                                aria-haspopup="true"/>
 
-                    <Menu ref="create_menu"
-                          :model="store.list_create_menu"
-                          :popup="true" />
-
-                    <!--/form_menu-->
-
+                        <Menu ref="create_menu" :model="store.list_create_menu" :popup="true" />
+                        <!-- /form_menu -->
                     </div>
-
                 </template>
 
-                    <Actions/>
-
-                <Table/>
-
+                <Actions />
+                <Table />
             </Panel>
         </div>
 
-         <Filters/>
+        <Filters />
+        <DoctorFilter />
+        <RouterView />
 
-        <DoctorFilter/>
-        <RouterView/>
 
+        <Dialog header="Upload Doctors CSV" :visible.sync="isDialogVisible" modal
+
+             :closable="true"
+                :dismissable-mask="true"
+                :close-on-escape="true"
+           >
+            <div class="p-fluid">
+                <input type="file" ref="fileInput" @change="handleFileUpload" accept=".csv" />
+            </div>
+            <template #footer>
+                <Button label="Close" @click="isDialogVisible = false" class="p-button-text" />
+            </template>
+        </Dialog>
     </div>
-
-
 </template>
