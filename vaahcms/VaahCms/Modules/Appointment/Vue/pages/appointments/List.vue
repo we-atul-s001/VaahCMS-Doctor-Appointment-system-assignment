d7e0123e59e98eb0aa2ec220bf29c08fa964bea3
Atul Pratap Singh <atul-s001@webreinvent.com>
2024-10-28 12:46:28 +0530

    Updated: Dashboard layout & mentioned required in the headers

--- vaahcms/VaahCms/Modules/Appointment/Vue/pages/appointments/List.vue
+++ vaahcms/VaahCms/Modules/Appointment/Vue/pages/appointments/List.vue
@@ -290,12 +290,16 @@ const generatePreviewData = (data, selected_headers) => {
                                     <h3>Database Headers</h3>
                                     <div class="database-header-container">
                                         <div v-for="(field, index) in store.assets.fields" :key="index" class="header-row">
-                                            <span class="database-header">{{ field }}</span>
+                <span class="database-header">
+                    {{ field }}
+                    <span v-if="index < 6" class="required-star">*</span>
+                </span>
                                         </div>
                                     </div>
                                 </div>
 
-                                <div class="column">
+
+                            <div class="column">
                                     <h3>Extracted Headers</h3>
                                     <div v-if="headers.length > 0">
                                         <div v-for="(dbHeader, index) in store.assets.fields" :key="index">
@@ -488,4 +492,9 @@ h2 {
 .mapping-summary {
     margin: 20px 0;
 }
+.required-star {
+    color: red;
+    margin-left: 4px;
+}
+
 </style>
